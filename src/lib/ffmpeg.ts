5a757e3b628583b5c01b7706d5f7fe362b880a4b
FedYou <federicochurruarin0@gmail.com>
2025-10-30 22:27:23 -0300

    feat: add functions to use ffmpeg internally

---
+++ src/lib/ffmpeg.ts
@@ -0,0 +1,86 @@
+import { execAsync } from 'utils/exec'
+// ----------------------------
+// --- Types ------------------
+// ----------------------------
+
+interface VideoOptions {
+  entryVideo: string
+  entryAudio: string
+  outFile: string
+  type: 'mp4' | 'webm'
+  metadata?: {
+    [key: string]: string | number | boolean
+  }
+}
+
+// ----------------------------
+// --- Variables --------------
+// ----------------------------
+const COMMAND = 'ffmpeg'
+
+const COMMAND_ARGS = {
+  //
+  AUDIO_WEBM: ['-map', '0:a', '-c:a', 'libvorbis', '-f', 'webm', '-y'],
+  AUDIO_MP3: ['-map', '0:a', '-c:a', 'libmp3lame', '-q:a', '0', '-f', 'mp3', '-y'],
+  AUDIO_ACC: ['-map', '0:a', '-c:a', 'aac', '-f', 'mp4', '-y'],
+  VIDEO: ['-c:v', 'copy', '-c:a', 'copy', '-y']
+}
+
+// ----------------------------
+// --- Functions --------------
+// ----------------------------
+
+// Add metadata to ffmpeg args
+function metadataToArgs(metadata: any) {
+  const args: (string | number)[] = []
+
+  for (const key in metadata) {
+    args.push('-metadata')
+    args.push(`${key}="${metadata[key]}"`)
+  }
+
+  return args
+}
+
+// Base function to convert audio
+async function toAudio(
+  entryFile: string,
+  outFile: string,
+  type: 'mp3' | 'aac' | 'webm',
+  metadata?: any
+) {
+  const args: (string | number)[] = ['-i', entryFile]
+
+  if (metadata) args.push(...metadataToArgs(metadata))
+  if (type === 'webm') args.push(...COMMAND_ARGS.AUDIO_WEBM)
+  else if (type === 'aac') args.push(...COMMAND_ARGS.AUDIO_ACC)
+  else if (type === 'mp3') args.push(...COMMAND_ARGS.AUDIO_MP3)
+  args.push(outFile)
+
+  await execAsync(COMMAND, args)
+}
+
+async function toAudioMp3(entryFile: string, outFile: string, metadata?: any) {
+  return await toAudio(entryFile, outFile, 'mp3', metadata)
+}
+
+async function toAudioWebm(entryFile: string, outFile: string) {
+  return await toAudio(entryFile, outFile, 'webm')
+}
+
+async function toAudioAac(entryFile: string, outFile: string) {
+  return await toAudio(entryFile, outFile, 'aac')
+}
+
+async function toVideo({ entryVideo, entryAudio, outFile, metadata, type }: VideoOptions) {
+  const args: (string | number)[] = ['-i', entryVideo, '-i', entryAudio]
+
+  if (metadata) args.push(...metadataToArgs(metadata))
+  args.push(...COMMAND_ARGS.VIDEO)
+  args.push('-f', type)
+  args.push(outFile)
+
+  await execAsync(COMMAND, args)
+}
+
+export default { toAudioMp3, toAudioWebm, toAudioAac, toVideo }

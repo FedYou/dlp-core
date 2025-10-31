d787d07afd42c8d95e5406735d19f485b4dded8b
FedYou <federicochurruarin0@gmail.com>
2025-10-31 13:27:12 -0300

    fix: fix Types

--- types/media.d.ts
+++ types/media.d.ts
@@ -1,5 +1,43 @@
-import type { DataOptions } from './enum'
 import type { FileDownloadProgress } from './dlf'
+
+export interface DataOptions {
+  /**
+   * Platform
+   */
+  platform: 'youtube' | 'instagram' | 'tiktok'
+  /**
+   * Type of download
+   *
+   * Youtube: onlyVideo | onlyAudio | video
+   *
+   * Instagram: onlyVideo | onlyAudio | video
+   *
+   * Tiktok: video
+   */
+  type: 'onlyVideo' | 'onlyAudio' | 'video'
+  /**
+   * Id of video
+   */
+  id: string
+  /**
+   * Language of youtube audio
+   */
+  audioLanguage?: string
+  /**
+   * Type of video
+   *
+   * Youtube: mp4 | webm
+   *
+   * Instagram: mp4
+   *
+   * Tiktok: mp4
+   */
+  videoType?: 'mp4' | 'webm'
+  /**
+   * Index position of format video
+   */
+  videoQuality?: number
+}
 
 export interface MediaDownloadOn {
   start: (type: 'video' | 'audio' | 'thumbnail') => void

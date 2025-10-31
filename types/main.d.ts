c59d9760e4106eb0bc46f293891dc954e84a6dc6
FedYou <federicochurruarin0@gmail.com>
2025-10-31 01:51:00 -0300

    docs: update Typescript definitions

--- types/main.d.ts
+++ types/main.d.ts
@@ -1,21 +1,14 @@
 import type { JSONIG, JSONYT, JSONTK } from './json'
 import type { MediaProcessOptions, MediaDownloadOptions } from './media'
 
-declare module 'dlp-core' {
-  export function getJSON(url: string): Promise<any | null>
-  export function getJSONYT(url: string): Promise<JSONYT | null>
-  export function getJSONIG(url: string): Promise<JSONIG | null>
-  export function getJSONTK(url: string): Promise<JSONTK | null>
-  export function downloadMedia(options: MediaDownloadOptions): Promise<void>
-  export function processMedia(options: MediaProcessOptions): Promise<string>
-
-  const _default: {
-    getJSON: typeof getJSON
-    getJSONYT: typeof getJSONYT
-    getJSONIG: typeof getJSONIG
-    getJSONTK: typeof getJSONTK
-    downloadMedia: typeof downloadMedia
-    processMedia: typeof processMedia
-  }
-  export default _default
+declare namespace core {
+  function getJSON(url: string): Promise<any | null>
+  function getJSONYT(url: string): Promise<JSONYT | null>
+  function getJSONIG(url: string): Promise<JSONIG | null>
+  function getJSONTK(url: string): Promise<JSONTK | null>
+  function downloadMedia(options: MediaDownloadOptions): Promise<void>
+  function processMedia(options: MediaProcessOptions): Promise<string>
 }
+
+export = core
+export as namespace core

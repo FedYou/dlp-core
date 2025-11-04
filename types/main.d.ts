dfe3a5692707327eb06841eda831ea7c2375b61a
FedYou <federicochurruarin0@gmail.com>
2025-11-04 20:37:15 -0300

    docs(types): update types on main

--- types/main.d.ts
+++ types/main.d.ts
@@ -1,10 +1,30 @@
-import type { DataOptions, MediaDownloadOn, MediaProcessOn } from './media'
+import type { DataOptions, MediaDownloadOn, MediaProcessOn } from './media.'
+import type { VideoInfo, Metadata } from './any'
+import type { FormatVideoDefault, FormatAudioDefault, FormatVideoYT, FormatAudioYT } from './json'
+
+interface SaveMediaOptions {
+  dir: string
+  fileName: string
+  cover?: boolean = false
+  metadata?: boolean = false
+}
 
 declare namespace core {
   class DLP {
     async addURL(url: string): Promise<void>
-    async downloadMedia(options: DataOptions, on?: MediaDownloadOn): Promise<void>
-    async processMedia(options: DataOptions, on?: MediaProcessOn): Promise<string>
+    async getMedia(options: DataOptions): Promise<void>
+    async saveMedia(options: SaveMediaOptions): Promise<void>
+    get info(): VideoInfo | null
+    get formats(): {
+      audio?:
+        | FormatAudioDefault[]
+        | FormatAudioYT
+        | FormatAudioDefault
+        | { [key: string]: FormatAudioYT[] }
+        | null
+      mp4: FormatVideoDefault[] | FormatVideoYT[]
+      webm?: FormatVideoYT[] | null
+    }
   }
 }
 
